94e18ab40041f74d91a854ffceb3a6a8a186ed16
Pavan kalyan Vallem <116752882+vallempavan2002@users.noreply.github.com>
2023-02-28 22:22:12 +0530

    operators.java

---
+++ pavan operators.java
@@ -0,0 +1,12 @@
+public class Main
+{
+    public static void main(String[]args)
+    {
+        int a=20,b=30;
+        System.out.println("a+b="+(a+b));
+        System.out.println("a-b="+(a-b));
+         System.out.println("a%b="+(a%b));
+        System.out.println("a/b="+(a/b));
+    }
+}
+

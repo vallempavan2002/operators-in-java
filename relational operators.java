439b6445cd410419078925116282545ec81afa92
Pavan kalyan Vallem <116752882+vallempavan2002@users.noreply.github.com>
2023-02-28 22:27:50 +0530

    relational operators.java

---
+++ relational operators.java
@@ -0,0 +1,13 @@
+public class Main
+{
+    public static void main(String args[])
+    {
+        int a=20,b=30;
+        System.out.println(a==b);
+       System.out.println(a>b);
+        System.out.println(a<b);
+         System.out.println(a<=b);
+       System.out.println(a>=b);
+        System.out.println(a!=b);
+    }
+}
